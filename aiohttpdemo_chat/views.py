26e954129e6c32f8f970a14752b697c669e8d0e2
Michal Povinsky <antihutka@gmail.com>
2019-07-23 05:37:21 +0200

    Fixes for running behind proxy

--- aiohttpdemo_chat/views.py
+++ aiohttpdemo_chat/views.py
@@ -7,7 +7,22 @@ import asyncio
 
 log = logging.getLogger(__name__)
 
+trusted = ['192.168.16.5', '127.0.0.1']
+
+def get_true_remote(request):
+  log.info("rem %s", repr(request.remote))
+  if request.remote in trusted and 'X-Forwarded-For' in request.headers:
+    fw = request.headers['X-Forwarded-For']
+    log.info("fwd %s", repr(fw))
+    fw = [x.strip() for x in fw.split(',')]
+    while len(fw) > 1 and fw[-1] in trusted:
+      del fw[-1]
+    return fw[-1]
+  else:
+    return request.remote
+
 async def index(request):
+    #print(request.__dict__)
     ws_current = web.WebSocketResponse()
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
@@ -23,9 +38,10 @@ async def index(request):
       return asyncio.get_event_loop().run_in_executor(dbexec, lambda: func(*args, **kwargs))
 
     nameid, name = await dbrun(db.get_name)
-    log.info('%s joined from %s.', name, request.remote)
+    rmt = get_true_remote(request)
+    log.info('%s joined from %s.', name, rmt)
 
-    await dbrun(db.log_login, nameid, request.remote)
+    await dbrun(db.log_login, nameid, rmt)
 
     await ws_current.send_json({'action': 'connect', 'name': name})
 
